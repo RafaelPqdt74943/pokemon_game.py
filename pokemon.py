3d195c7e1d3b0fc44fd01c4b0a8c51dba8b30750
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-10-17 21:57:46 -0300

    commit metodo para exibir nome e level

--- pokemon.py
+++ pokemon.py
@@ -1,10 +1,16 @@
 class Pokemon :
-    def __init__(self, tipo, especie):
+    def __init__(self, tipo, especie, level = 1, nome=None):
         self.tipo =tipo
         self.especie = especie
+        self.level = level
+        
+        if nome: 
+            self.nome = nome
+        else : 
+            self.nome = especie
         
     def __str__ (self):
-        return "{} ({})".format(self.especie, self.tipo)
+        return "{} ({})".format(self.nome, self.level)
     
     def atacar(self,pokemon):
         print("{} atacou {}".format(self, pokemon))
@@ -13,7 +19,7 @@ class Pokemon :
 
         
 
-meu_pokemon = Pokemon("fogo", "charmander")
+meu_pokemon = Pokemon("fogo", "charmander", nome="tunico")
 
 pokemon_meu_amigo = Pokemon("eletrico", "pikachu")
 
@@ -23,3 +29,7 @@ pokemon_meu_amigo.atacar(meu_pokemon)
 
 #print(meu_pokemon)
 #print(pokemon_meu_amigo)
+
+print(meu_pokemon)
+print(pokemon_meu_amigo)
+
