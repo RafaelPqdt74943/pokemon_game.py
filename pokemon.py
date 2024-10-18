407d33c85b227c7dbd601886d6f107606c6dff99
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-10-18 20:19:44 -0300

    criando classes para cada tipo de pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,5 @@
 class Pokemon :
-    def __init__(self, tipo, especie, level = 1, nome=None):
-        self.tipo =tipo
+    def __init__(self, especie, level = 1, nome=None):
         self.especie = especie
         self.level = level
         
@@ -15,16 +14,35 @@ class Pokemon :
     def atacar(self,pokemon):
         print("{} atacou {}".format(self, pokemon))
 
-class pokemon_eletrico(Pokemon):
+class pokemon_Eletrico(Pokemon):
+    tipo = "eletrico"
+    
+    
     def atacar(self, pokemon):
         print("{} lançou um raio do trovão em {}".format(self, pokemon))
-    def dar_choque(self, pokemon):
-        print("{} Deu choque em {}".format(self, pokemon))
         
+class pokemon_Fogo(Pokemon):
+    tipo = "fogo"
+    
+    
+    def atacar(self, pokemon):
+        print("{} lançou uma bola de fogo na cabeça de {}".format(self, pokemon))
         
-meu_pokemon = pokemon_eletrico("eletrico", "Pikachu")
-pokemon_amigo = Pokemon("fogo", "charmander")
 
-meu_pokemon.dar_choque(pokemon_amigo)
+class pokemon_Agua(Pokemon):
+    tipo = "agua"
+    
+    
+    def atacar(self, pokemon):
+        print("{} lançou umjato de d'águas em {}".format(self, pokemon))
+        
+        
+        
+meu_pokemon = pokemon_Fogo("charmander")
+pokemon_amigo = pokemon_Eletrico("Pikachu")
+
+print(meu_pokemon, meu_pokemon.tipo)
+print(pokemon_amigo, pokemon_amigo.tipo)
+
+meu_pokemon.atacar(pokemon_amigo)
 pokemon_amigo.atacar(meu_pokemon)
-
